e614644128f02f198bfbd8eab3ac94ce2ed40bd0
pittas <lexpittas@gmail.com>
2023-11-30 13:56:08 -0500

    Added an about widget and a photo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 
 df = pd.read_csv("spotify_data.csv", index_col=0)
 
@@ -40,3 +41,14 @@ if selected_song or selected_artist:
         selected_song_info = df[df['identifier'] == selected_identifier]
         # Display both song and artist information
         st.write(selected_song_info[['track_name', 'artist_name', 'year', 'genre']].squeeze())
+
+image = Image.open('spotify.jpg')
+
+#Create an about widget
+with st.sidebar:
+    st.title("We are Group 12 from WatSpeed")
+    st.text("And this is our song recommendation app")
+    st.image(image, caption='Spotify Genres')
+    st.header("Image Credit")
+    st.text("Photo by David Pupăză Unsplash")
+  
