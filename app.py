94d691ba94769e5072e4d9fde9b7af547c328483
Michael Racanelli <michaelracanelli@Michaels-MacBook-Air.local>
2023-11-29 22:48:53 -0500

    added artist search

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 import streamlit as st
 import pandas as pd
 
-
 df = pd.read_csv("spotify_data.csv", index_col=0)
 
 # Add a unique identifier for each song and artist combination
-# this is so when the user is selecting a specific song from the list after typing, they have artist info included
 df['identifier'] = df['track_name'] + ' - ' + df['artist_name']
+
+# Sort the DataFrame by popularity
+df = df.sort_values(by='popularity', ascending=False)
 
 # Streamlit app title header
 st.title('Song Finder')
 
-# Widget for user input
-selected_song = st.text_input('To begin, type in a song name:', value='', key='song_input', help='Make sure to include punctuation!')
+# Widget for user input (song name)
+selected_song = st.text_input('Type in a song name:', value='', key='song_input', help='Make sure to include punctuation!')
+
+# Widget for user input (artist name)
+selected_artist = st.text_input('Type in an artist name:', value='', key='artist_input')
 
 # Display information based on user input
-if selected_song:
+if selected_song or selected_artist:
     # Filter DataFrame based on user input
-    result = df[df['track_name'].str.contains(selected_song, case=False)]
+    result = df[
+        (df['track_name'].str.contains(selected_song, case=False)) &
+        (df['artist_name'].str.contains(selected_artist, case=False))
+    ]
 
     # Subheader for song select
     st.subheader('Select a song:')
@@ -32,4 +39,4 @@ if selected_song:
     if selected_identifier:
         selected_song_info = df[df['identifier'] == selected_identifier]
         # Display both song and artist information
-        st.write(selected_song_info[['track_name', 'artist_name','year','genre']].squeeze())
+        st.write(selected_song_info[['track_name', 'artist_name', 'year', 'genre']].squeeze())
